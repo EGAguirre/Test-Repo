f76cb3e908b0852b07026cb8f0c6e002a0d42792
EGAguirre <9eaguirre@stpatrick.org>
2016-10-06 11:52:01 -0500

    added an object with variables and a for loop.

---
+++ octocats.js
@@ -0,0 +1,22 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(n,a){
+  this.name = n;
+  this.arms = a;
+
+  this.slap = function(){
+    for(){
+      console.log("SLAP");
+    }
+  }
+}
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
+var youroctocat = new Octocat("Jimmy",8);
+var myoctocat = new Octocat("Whiskers",12);
+var hisoctocat = new Octocat("Squidward",6);
